245172de2bd1b4aa0ec6e3cccb5e681058b5010d
siri palakurthi <palakurthysiri@gmail.com>
2025-04-29 20:51:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,21 @@ const studentRoutes = require('./routes/studentRoutes');
 
 dotenv.config(); // Load environment variables from .env file
 
-const corsOptions = {
-  origin: 'https://sms-frontend-s9cb.onrender.com', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, 
-  optionsSuccessStatus: 204, 
-};
-
-app.use(cors(corsOptions));
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 console.log('MONGODB_URI from server.js:', process.env.MONGODB_URI);
 connectDB();
 
-app.use(cors({ origin: 'http://localhost:3000' })); // Allow requests from localhost:3000
+const corsOptions = {
+  origin: 'http://localhost:3000', // Your Render frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions)); // Apply CORS options for your Render frontend
+
+app.use(cors({ origin: 'http://localhost:3000' })); // Allow requests from localhost:3000 (for development)
 app.use(express.json());
 
 app.use('/students', studentRoutes);
